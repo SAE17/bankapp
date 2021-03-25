56713afaf6161a130720957216fea609bc12bb3d
sae17 <akmal.shoimardon@mail.ru>
2021-03-25 22:51:27 +0500

    Добавлено список карт доступных к оплате

--- pkg/bank/payment/example_test.go
+++ pkg/bank/payment/example_test.go
@@ -24,3 +24,38 @@ func ExampleMax() {
 	fmt.Println(res.ID)
 	// Output: 1
 }
+
+func ExamplePaymentSources() {
+	cards := []types.Card{
+		{
+			ID:      1000,
+			PAN:     "5058 xxxx xxxx 0001",
+			Balance: 200_00,
+			Active:  false,
+		},
+		{
+			ID:      1001,
+			PAN:     "5058 xxxx xxxx 0002",
+			Balance: 10_00,
+			Active:  true,
+		},
+		{
+			ID:      1002,
+			PAN:     "5058 xxxx xxxx 0003",
+			Balance: 1000_00,
+			Active:  false,
+		},
+		{
+			ID:      1003,
+			PAN:     "5058 xxxx xxxx 0004",
+			Balance: 0,
+			Active:  true,
+		},
+	}
+	sources := PaymentSources(cards)
+	for _, source := range sources {
+		fmt.Println(source.Number)
+	}
+	// Output:
+	// 5058 xxxx xxxx 0002
+}
